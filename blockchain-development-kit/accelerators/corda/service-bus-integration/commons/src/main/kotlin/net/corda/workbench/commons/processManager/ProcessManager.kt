1267339daa15a755eda6163847ea5d784ef56f51
Ian Morgan <ian.morgan@r3.com>
2018-12-20 13:37:42 +0000

    commons - working on better task executor & process manager

---
+++ blockchain-development-kit/accelerators/corda/service-bus-integration/commons/src/main/kotlin/net/corda/workbench/commons/processManager/ProcessManager.kt
@@ -0,0 +1,213 @@
+package net.corda.workbench.commons.processManager
+
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.concurrent.thread
+
+/**
+ * Keeps track of running processes
+ */
+
+
+
+class ProcessManager constructor(
+        private val outputSink: (String) -> Unit = { msg -> ProcessMonitor.consoleMessageSink(msg) },
+        private val errorSink: (String) -> Unit = { msg -> ProcessMonitor.consoleMessageSink(msg) },
+        private val processCompletedSink: (ProcessManager.ManagedProcess, Int) -> Unit = { _, _ -> }
+
+
+) {
+    private val sleepTime = 10
+    private var monitoring = false
+    private val processes = HashMap<Key, Process>()
+    private val processList = ArrayList<ManagedProcess>()
+    private val processMonitors = HashMap<UUID, ProcessMonitor>()
+
+    fun register(process: Process, id: UUID = UUID.randomUUID(), label: String = "") {
+        val mp = ManagedProcess(process, id, label)
+        processList.add(mp)
+        processMonitors[id] = ProcessMonitor(mp, outputSink, errorSink, processCompletedSink)
+    }
+
+    fun findByProcess(process: Process): ProcessInfo? {
+        val mp = processList.singleOrNull() { it.process == process }
+        return if (mp == null) null else mapToProcessInfo(mp)
+    }
+
+    fun findById(id: UUID): ProcessInfo? {
+        val mp = processList.singleOrNull { it.id == id }
+        return if (mp == null) null else mapToProcessInfo(mp)
+    }
+
+    fun findByLabel(label: String): ProcessInfo? {
+        val mp = processList.singleOrNull { it.label == label }
+        return if (mp == null) null else mapToProcessInfo(mp)
+    }
+
+    fun allProcesses(): List<ProcessInfo> {
+        return processList.map { mapToProcessInfo(it) }
+    }
+
+    fun register(network: String, task: String, process: Process) {
+        val key = Key(network, task)
+        println("Process $key registered ")
+        processes[key] = process
+    }
+
+    private fun mapToProcessInfo(mp: ManagedProcess): ProcessInfo {
+        val monitor = processMonitors[mp.id]!!
+        return ProcessInfo(process = mp.process, id = mp.id, label = mp.label, monitor = monitor)
+    }
+
+    fun monitor() {
+        if (!monitoring) {
+            println("Process monitor started")
+            monitoring = true
+            thread(isDaemon = true) {
+                while (monitoring) {
+                    println("Process monitor checking ${processes.size} processes...")
+                    processes.forEach {
+                        println("  process: ${it.key}, isAlive? ${it.value.isAlive}")
+                    }
+                    println("Process monitor sleeping for $sleepTime...")
+                    Thread.sleep(sleepTime * 1000L)
+                }
+                println("Process monitor stopped")
+            }
+        }
+    }
+
+    fun all(): List<Process> {
+        return ArrayList(processes.values)
+    }
+
+    fun queryForNetwork(network: String): List<ProcessStatus> {
+        val results = ArrayList<ProcessStatus>()
+        processes.entries.forEach {
+            if (it.key.network == network) {
+                results.add(ProcessStatus(it.key.processName, it.value.isAlive))
+            }
+        }
+        return results
+    }
+
+    fun queryForNodeOnNetwork(network: String, processName: String): Process? {
+        return processes[Key(network, processName)]
+    }
+
+    fun removeProcess(process: Process) {
+        // not the nicest code :(
+        processes.entries.forEach {
+            if (it.value == process) {
+                processes.remove(it.key)
+                println("Process ${it.key} removed")
+                return
+            }
+        }
+    }
+
+    fun killAll() {
+        for (p in processes.entries) {
+            println("Forcibly killing ${p.key}")
+            p.value.destroyForcibly()
+        }
+        processes.clear()
+    }
+
+    data class Key(val network: String, val processName: String)
+
+    data class ProcessStatus(val name: String, val isAlive: Boolean)
+
+    data class ManagedProcess(val process: Process, val id: UUID = UUID.randomUUID(), val label: String = "")
+
+    data class ProcessInfo(val process: Process, val id: UUID, val label: String, val monitor: ProcessMonitor)
+
+
+}
+
+
+class ProcessMonitor constructor(private val managedProcess: ProcessManager.ManagedProcess,
+                                 private val outputSink: (String) -> Unit = { msg -> consoleMessageSink(msg) },
+                                 private val errorSink: (String) -> Unit = { msg -> consoleMessageSink(msg) },
+                                 private val processCompletedSink: (ProcessManager.ManagedProcess, Int) -> Unit = { _, _ -> }
+
+) {
+    var exitCode: Int? = null
+
+    init {
+        val outputMonitor = monitorOutput()
+        val errorMonitor = monitorError()
+        monitorRunning(outputMonitor, errorMonitor)
+    }
+
+    private fun monitorOutput(): Thread {
+        return thread() {
+            val br = BufferedReader(InputStreamReader(managedProcess.process.inputStream))
+            var line: String? = br.readLine()
+            do {
+                while (line != null) {
+
+                    outputSink.invoke(line)
+                    line = br.readLine()
+                }
+                Thread.sleep(1000)
+            } while (true)
+        }
+    }
+
+    private fun monitorError(): Thread {
+        return thread() {
+            val br = BufferedReader(InputStreamReader(managedProcess.process.errorStream))
+            var line: String? = br.readLine()
+            do {
+                while (line != null) {
+                    errorSink.invoke(line)
+                    line = br.readLine()
+                }
+                Thread.sleep(1000)
+            } while (true)
+        }
+    }
+
+    private fun monitorRunning(outputMonitor: Thread, errorMonitor: Thread) {
+        thread() {
+            do {
+                if (managedProcess.process.isAlive) {
+                    Thread.sleep(1000)
+                } else {
+                    exitCode = managedProcess.process.exitValue()
+                    outputSink("${managedProcess.id} has completed with exit code $exitCode")
+                    processCompletedSink.invoke(managedProcess, exitCode as Int)
+
+
+                    //  fixme !
+                    outputMonitor.stop()
+                    errorMonitor.stop()
+
+                    break;
+                }
+
+            } while (true)
+        }
+    }
+
+    fun isRunning(): Boolean {
+        return exitCode == null
+    }
+
+    fun exitCode(): Int {
+        return exitCode!!
+    }
+
+    /**
+     * A default sink (consumer) of messages that just prints to the console
+     */
+    companion object {
+        fun consoleMessageSink(m: String) {
+            println(m)
+        }
+    }
+}
--- blockchain-development-kit/accelerators/corda/service-bus-integration/commons/src/main/kotlin/net/corda/workbench/commons/taskManager/Executor.kt
+++ blockchain-development-kit/accelerators/corda/service-bus-integration/commons/src/main/kotlin/net/corda/workbench/commons/taskManager/Executor.kt
@@ -1,26 +1,41 @@
 package net.corda.workbench.commons.taskManager
 
-import java.io.OutputStream
+
+import java.util.*
 
 /**
  * A simple execution context to control where messages and logging are sent to
  */
-class ExecutionContext(messageSink: (String) -> Unit = { consoleMessageSink(it) }) {
+class ExecutionContext(messageSink: (String) -> Unit = { consoleMessageSink(it) },
+                       outputSink: (String) -> Unit = { consoleMessageSink(it) },
+                       errorSink: (String) -> Unit = { consoleMessageSink(it) },
+                       id: UUID = UUID.randomUUID()
+) {
+
+    val id: UUID = id
 
     /**
-     * Standard output stream. Tasks should redirect any output to this stream
+     * Standard output stream. Tasks should redirect any application output to this sink
      */
-    val outputStream: OutputStream = System.out
+    //val outputStream: OutputStream = System.out
+
+    val outputSink: (String) -> Unit = outputSink
+
 
     /**
-     * Standard error stream. Tasks should redirect any error to this stream
+     * Standard error stream. Tasks should redirect any application errors to this sink
      */
-    val errorStream: OutputStream = System.err
+    val errorSink: (String) -> Unit = errorSink
+
 
     /**
      * Dedicated stream for the task's internal reporting. Should
      * be limited to basic status and progress messages
      */
+    val messageSink: (String) -> Unit = messageSink
+
+
+    @Deprecated(message = "use messageSink instead")
     val messageStream: (String) -> Unit = messageSink
 
     /**
@@ -39,31 +54,70 @@ class ExecutionContext(messageSink: (String) -> Unit = { consoleMessageSink(it) 
  */
 class TaskExecutor(private val taskLogMessageSink: (TaskLogMessage) -> Unit) {
 
+    val executorId: UUID = UUID.randomUUID()
+
     fun exec(t: Task) {
 
         // wire up an execution context linked to this taskLogMessageSink, so that
         // executionContext.messageStream is sent via TaskLogMessage
-        val sink = MessageSink(t, taskLogMessageSink)
-        val executionContext = ExecutionContext({ sink.doit(it) })
+        val sink = MessageSink(executorId, t, taskLogMessageSink)
+        val executionContext = ExecutionContext({ message -> sink.doit(message) })
 
-        taskLogMessageSink.invoke(TaskLogMessage("Starting ${t::class.java.simpleName}", t.taskID))
+        //sink.doit("Starting ${t::class.java.simpleName}")
+
+        taskLogMessageSink.invoke(TaskLogMessage(executorId, "Starting ${t::class.java.simpleName}", t.taskID))
         try {
             t.exec(executionContext)
-            taskLogMessageSink.invoke(TaskLogMessage("Completed ${t::class.java.simpleName}", t.taskID))
+            taskLogMessageSink.invoke(TaskLogMessage(executorId, "Completed ${t::class.java.simpleName}", t.taskID))
 
         } catch (ex: Exception) {
-            taskLogMessageSink.invoke(TaskLogMessage("Failed ${t::class.java.simpleName}", t.taskID))
-            taskLogMessageSink.invoke(TaskLogMessage("Exception is: ${ex.message}", t.taskID))
+            taskLogMessageSink.invoke(TaskLogMessage(executorId, "Failed ${t::class.java.simpleName}", t.taskID))
+            taskLogMessageSink.invoke(TaskLogMessage(executorId, "Exception is: ${ex.message}", t.taskID))
         }
     }
 
-    class MessageSink(val t: Task, private val taskLogMessageSink: (TaskLogMessage) -> Unit) {
+    class MessageSink(val executorId: UUID, val t: Task, private val taskLogMessageSink: (TaskLogMessage) -> Unit) {
         fun doit(message: String) {
-            val taskLogMessage = TaskLogMessage(message, t.taskID)
+            val taskLogMessage = TaskLogMessage(executorId, message, t.taskID)
             taskLogMessageSink.invoke(taskLogMessage)
         }
     }
 }
+
+
+/**
+ * Executes a single task with logging
+ */
+class TaskExecutor2(private val taskLogMessageSink: (TaskLogMessage) -> Unit) {
+
+    val executorId: UUID = UUID.randomUUID()
+
+    fun exec(t: Task) {
+
+        // wire up an execution context linked to this taskLogMessageSink, so that
+        // executionContext.messageStream is sent via TaskLogMessage
+        val sink = MessageSink(executorId, t, taskLogMessageSink)
+        val executionContext = ExecutionContext({ sink.doit(it) })
+
+        taskLogMessageSink.invoke(TaskLogMessage(executorId, "Starting ${t::class.java.simpleName}", t.taskID))
+        try {
+            t.exec(executionContext)
+            taskLogMessageSink.invoke(TaskLogMessage(executorId, "Completed ${t::class.java.simpleName}", t.taskID))
+
+        } catch (ex: Exception) {
+            taskLogMessageSink.invoke(TaskLogMessage(executorId, "Failed ${t::class.java.simpleName}", t.taskID))
+            taskLogMessageSink.invoke(TaskLogMessage(executorId, "Exception is: ${ex.message}", t.taskID))
+        }
+    }
+
+    class MessageSink(val executorId: UUID, val t: Task, private val taskLogMessageSink: (TaskLogMessage) -> Unit) {
+        fun doit(message: String) {
+            val taskLogMessage = TaskLogMessage(executorId, message, t.taskID)
+            taskLogMessageSink.invoke(taskLogMessage)
+        }
+    }
+}
+
 
 /**
  * Execute a list of tasks, waiting until the last one completes
