fda57f971c2b68205241beaff465a5c9141e03c7
Ian Morgan <ian.morgan@r3.com>
2019-01-15 10:15:21 +0000

    cleaning up build and adding master 'buildAll.sh' scripts

--- blockchain-development-kit/accelerators/corda/cordapps/basic-provenance/buildLocal.sh
+++ blockchain-development-kit/accelerators/corda/cordapps/basic-provenance/buildLocal.sh
@@ -4,4 +4,4 @@
 # Build and copy cordapp to the pre-installed examples
 #
 ./gradlew clean test assemble
-cp cordapp/build/libs/cordapp-example-0.1.jar lib/basic-provenance.jar
+cp cordapp/build/libs/cordapp-example-0.1.jar ../jars/basic-provenance.jar
--- blockchain-development-kit/accelerators/corda/cordapps/simple-marketplace/buildLocal.sh
+++ blockchain-development-kit/accelerators/corda/cordapps/simple-marketplace/buildLocal.sh
@@ -4,4 +4,4 @@
 # Build and copy cordapp to the pre-installed examples
 #
 ./gradlew clean test assemble
-cp cordapp/build/libs/cordapp-simpleMarketplace-0.1.jar lib/simple-marketplace.jar
+cp cordapp/build/libs/cordapp-simpleMarketplace-0.1.jar ../jars/simple-marketplace.jar
