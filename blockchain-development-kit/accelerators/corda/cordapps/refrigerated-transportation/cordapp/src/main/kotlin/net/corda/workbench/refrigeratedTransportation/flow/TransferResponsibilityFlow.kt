dda84bb52ad565f2e56cf0a6685b737194f6fb18
Manuel Borsani <Manuel.Borsani@cegeka.it>
2019-01-30 15:15:49 +0100

    add progress Tracker

--- blockchain-development-kit/accelerators/corda/cordapps/refrigerated-transportation/cordapp/src/main/kotlin/net/corda/workbench/refrigeratedTransportation/flow/TransferResponsibilityFlow.kt
+++ blockchain-development-kit/accelerators/corda/cordapps/refrigerated-transportation/cordapp/src/main/kotlin/net/corda/workbench/refrigeratedTransportation/flow/TransferResponsibilityFlow.kt
@@ -16,6 +16,7 @@ import net.corda.core.node.services.queryBy
 import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
 import net.corda.workbench.refrigeratedTransportation.RefrigerationContract
 import net.corda.workbench.refrigeratedTransportation.Shipment
 import net.corda.workbench.refrigeratedTransportation.Transfer
@@ -24,14 +25,19 @@ import net.corda.workbench.refrigeratedTransportation.Transfer
 @StartableByRPC
 class TransferResponsibilityFlow(private val linearId: UniqueIdentifier, private val transfer: Transfer) : FlowLogic<SignedTransaction>() {
 
+    override val progressTracker =TransferResponsibilityFlow.tracker()
+
     @Suspendable
     override fun call(): SignedTransaction {
 
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
 
-        // Retrieve the shipment from the vault.
+
+        progressTracker.currentStep=QUERY_VAULT_BY_LINEARID
         val queryCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
         val items = serviceHub.vaultService.queryBy<Shipment>(queryCriteria).states
+
+
         if (items.isEmpty()) {
             throw IllegalArgumentException("Cannot find a shipment for $linearId")
         }
@@ -47,7 +53,7 @@ class TransferResponsibilityFlow(private val linearId: UniqueIdentifier, private
             throw IllegalArgumentException("Transfer cannot be to the same counterparty")
         }
 
-        // build txn
+        progressTracker.currentStep = GENERATING_TRANSACTION
         val cmd = Command(RefrigerationContract.Commands.Transfer(),
                 shipment.participants.map { it -> it.owningKey })
         val builder = TransactionBuilder(notary = notary)
@@ -55,26 +61,59 @@ class TransferResponsibilityFlow(private val linearId: UniqueIdentifier, private
                 .addOutputState(shipment.transferResponsibility(transfer.newCounterparty), RefrigerationContract.ID)
                 .addCommand(cmd)
 
-        // verify
+
+        progressTracker.currentStep = VERIFYING_TRANSACTION
         builder.verify(serviceHub)
+
+
+        progressTracker.currentStep = SIGNING_TRANSACTION
         val ptx = serviceHub.signInitialTransaction(builder)
 
-        // make sure everyone signs
+
+        progressTracker.currentStep = GATHERING_SIGS
         val sessions = (shipment.participants - ourIdentity).map { initiateFlow(it) }.toSet()
-        val stx = subFlow(CollectSignaturesFlow(ptx, sessions))
+        val stx = subFlow(CollectSignaturesFlow(ptx, sessions,GATHERING_SIGS.childProgressTracker()))
 
-        // complete and notarise
-        val finalTx = subFlow(FinalityFlow(stx))
 
-        // let the observer know
+        progressTracker.currentStep = FINALISING_TRANSACTION
+        val finalTx = subFlow(FinalityFlow(stx,FINALISING_TRANSACTION.childProgressTracker()))
+
+
+        progressTracker.currentStep=OBSERVER
         subFlow(ReportToObserverFlow(shipment.supplyChainObserver, finalTx))
+
+
         return finalTx
     }
+
+    companion object {
+        object QUERY_VAULT_BY_LINEARID : ProgressTracker.Step("Retrive the shipment from vault")
+        object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction based on new IOU.")
+        object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contract constraints.")
+        object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.")
+        object OBSERVER : ProgressTracker.Step("Let the observer know")
+        object GATHERING_SIGS : ProgressTracker.Step("Gathering the counterparty's signature.") {
+            override fun childProgressTracker() = CollectSignaturesFlow.tracker()
+        }
+
+        object FINALISING_TRANSACTION : ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+            override fun childProgressTracker() = FinalityFlow.tracker()
+        }
+
+        fun tracker() = ProgressTracker(
+                QUERY_VAULT_BY_LINEARID,
+                GENERATING_TRANSACTION,
+                VERIFYING_TRANSACTION,
+                SIGNING_TRANSACTION,
+                GATHERING_SIGS,
+                FINALISING_TRANSACTION,
+                OBSERVER
+        )
+    }
+
+
 }
 
-/**
- *
- */
 @InitiatedBy(TransferResponsibilityFlow::class)
 class TransferResponsibilityFlowResponder(val flowSession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
